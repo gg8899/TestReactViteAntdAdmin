d70b5a17a1a627353b91f78620e5a8b3a0b16aa0
GUOKKK <528032175@qq.com>
2023-12-19 22:36:06 +0800

    TODO 本周完成的TODO

--- src/mock/orderList.tsx
+++ src/mock/orderList.tsx
@@ -12,7 +12,19 @@ for (let index = 0; index < 20; index++) {
     address3: '局域网 局域网',
   });
 }
+/*
+  TODO 
+  定义 mock 文件结构 Page 列表。  使用 ts 来定义文档的结构
+  然后请求和 axios 的请求结构。
+  用户登录的情况，跨域请求处理后续=-====如何对接。。
+  利用 json-server 这个服务来发起
 
+  同时。完成订单列表和文件上传下载的使用回显预览封装等操作。
+  TODO 
+  查看最新的 antd 版本 实现表单新增删除的控制，数据的获取 是如何实现的。
+  TODO
+  数据可视化实现。学习看视频 实现 echarts 的首页，同时展示数据，实现进入之后重定向到 home 页面
+*/
 const logList = {
   total: 31,
   page: 1,
