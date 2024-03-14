06420c2c375a676afb642ad9d4643ed16c83261c
GUOKKK <528032175@qq.com>
2024-03-14 12:55:19 +0800

    路由配置json化优化

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,26 @@ import { lazy } from "react";
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App.tsx'
 import ErrorPage from '../pages/ErrorPage/index.tsx'
+import funtions from '../function.json'
 const Home = lazy(() => import('../pages/Home/index.tsx'));
 const OrderList = lazy(() => import('../pages/OrderList/index.tsx'));
 const Other = lazy(() => import('../pages/Other/index.tsx'));
 const PlayGames = lazy(() => import('../pages/PlayGames/index.tsx'))
 
+console.log(funtions)
+const getRoutes = (items) => {
+  if (!Array.isArray(items)) return;
+  return items.map(item => {
+    const Comp = lazy(() => import(`../pages/${item.element}/index.tsx`))
+    return ({
+      key: item.path,
+      path: item.path,
+      element: item.path === '/' ? <App /> : <Comp />,
+      children: getRoutes(item?.children)
+    })
+  })
+}
+// console.log(getRoutes(funtions));
 
 const routes = [
   {
@@ -28,6 +43,7 @@ const routes = [
         element: <Other />,
       }
     ],
+    // children
   },
   {
     path: '/playGames',
@@ -36,7 +52,7 @@ const routes = [
 ]
 
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(getRoutes(funtions));
 
 export default router;
 
